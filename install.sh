c0bfaf466f7faadae22c5e65d475723dca075aa6
SleepyStew <75189901+SleepyStew@users.noreply.github.com>
2023-01-06 23:09:43 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.12.2"
 export SCRIPT_RELEASE="v0.12.2"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/SleepyStew/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
